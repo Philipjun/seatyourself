d3a74fe9332d4ace58f05b17cd9be6549cfb9d64
Philip Jun <inbox@philipjun.com>
2014-04-24 20:58:52 -0400

    nav bar color change

---
+++ app/assets/javascripts/example.js
@@ -0,0 +1,25 @@
+// for var(i = 0; i <= 20; I++) {
+//   if(i % 2 ==0 ) {
+//     console.log(i + "is even");
+//   } else {
+//     console.log(i + "is odd");
+//   }
+
+// }
+
+
+// var day = new Date().getDay();
+// var x;
+
+// switch (day) {
+//   case 0;
+//   x= "Today its Sunday";
+//   break;
+//   case 1;
+//   x= "Today its Monday";
+//   break;
+//   case 2;
+//   x= "Today its Tuesday";
+//   break;
+
+// }
